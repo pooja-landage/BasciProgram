830f2ca4ebc34cc022d8c3bb1bd7aa7d67368273
pooja-landage <poojalandage22@gmail.com>
2021-09-16 07:29:46 +0530

    Add project files.

---
+++ BasciProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasciProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Welcome to World of C#");
+            Console.WriteLine("Enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("hello " + name);
+
+        }
+    }
+}
